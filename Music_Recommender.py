f5a480f1db3b6726a1cddb63fa3bb3bf1712e31d
Arthur Pulini <arthurpulini@gmail.com>
2024-02-23 09:29:35 -0300

    Without comments

--- Music_Recommender.py
+++ Music_Recommender.py
@@ -21,7 +21,6 @@ datas = pd.read_csv('/home/arthur-pulini/Documentos/Programação/Machine learni
 datasMusicalGenre = pd.read_csv('/home/arthur-pulini/Documentos/Programação/Machine learning Alura/Alura-Spotify/data_by_genres.csv')
 datasYears = pd.read_csv('/home/arthur-pulini/Documentos/Programação/Machine learning Alura/Alura-Spotify/data_by_year.csv')
 
-#Tratando os dados manualmente
 print(datas.head(2))
 print(datas["year"].unique())
 print(datas.shape)
@@ -36,7 +35,7 @@ print(datasMusicalGenre.isnull().sum(), "\n\n",  datasMusicalGenre.isna().sum())
 
 print(datasYears.head(2))
 print(datasYears["year"].unique())
-datasYears = datasYears[datasYears["year"]>=2000] #igualando aos anos da tabela datas
+datasYears = datasYears[datasYears["year"]>=2000] 
 print(datasYears["year"].unique())
 datasYears = datasYears.drop(["key", "mode"], axis=1)
 print(datasYears.head(2))
@@ -45,12 +44,9 @@ print(datasYears.head(2))
 print(datasYears.shape)
 print(datasMusicalGenre.isnull().sum(), "\n\n",  datasMusicalGenre.isna().sum())
 
-#Análise gráfica
-#A análise do Loudness será feita separada, pois, o range é de -60 a 0 db, sendo assim, será difícil a comparação com os outros dados
 fig = px.line(datasYears, x="year", y="loudness", markers=True, title="Loudness variation according to the years")
 #fig.show()
 
-#Fazendo a análise do ano com os outros dados
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=datasYears['year'], y=datasYears['acousticness'],
                     name='Acousticness'))
@@ -68,47 +64,41 @@ fig.add_trace(go.Scatter(x=datasYears['year'], y=datasYears['speechiness'],
                     name='Speechiness'))
 #fig.show()
 
-datasV1 = datas.drop(["artists", "name", "artists_song", "id"], axis=1) #Para fazer a matriz de correlação foi necessário dropar as colunas não numéricas
+datasV1 = datas.drop(["artists", "name", "artists_song", "id"], axis=1) 
 print(datasV1)
-fig = px.imshow(datasV1.corr(), text_auto=True)#Aplicando a matriz de correlação
+fig = px.imshow(datasV1.corr(), text_auto=True)
 #fig.show()
 
-#Analisando o datasMusicalGenre, para ver se os gêneros se repetem
 print(datasMusicalGenre['genres'].value_counts().sum())
-datasMusicalGenreV1 = datasMusicalGenre.drop(['genres'], axis=1)#Para fazer o cluster é necessário dropar as colunas não numéricas
+datasMusicalGenreV1 = datasMusicalGenre.drop(['genres'], axis=1)
 print(datasMusicalGenreV1)
 
-#Configurando o Pipeline com o StandartScaler e PCA
 pcaPipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=2, random_state=SEED))])
 
-#Aplicando o Pipeline nos dados por gêneros tratado
 genreEmbeddingPca = pcaPipeline.fit_transform(datasMusicalGenreV1)
-projection = pd.DataFrame(columns=['x', 'y'], data = genreEmbeddingPca)#Criando um DataFrame, a partir dos dados por gêneros transformados
+projection = pd.DataFrame(columns=['x', 'y'], data = genreEmbeddingPca)
 print(projection)
 
-#Configurando o KMeans
 kmeansPca = KMeans(n_clusters=5, verbose=True, random_state=SEED)
 
-#Aplicando KMeans
 kmeansPca.fit(projection)
 
-datasMusicalGenre['cluster_PCA'] = kmeansPca.predict(projection)#Adicionando o número dos clusters em datasMusicalGenre
-projection['cluster_PCA'] = kmeansPca.predict(projection)#Adicionando o número dos clusters em projection
-projection['genres'] = datasMusicalGenre['genres']#Aplicando a coluna genres em projection
+datasMusicalGenre['cluster_PCA'] = kmeansPca.predict(projection)
+projection['cluster_PCA'] = kmeansPca.predict(projection)
+projection['genres'] = datasMusicalGenre['genres']
 print(projection)
 
-fig = px.scatter(projection, x='x', y='y', color='cluster_PCA', hover_data=['x', 'y', 'genres'])#hover_data=['x', 'y', 'genres'] informa qual o x, y e gênero o ponto pertence
+fig = px.scatter(projection, x='x', y='y', color='cluster_PCA', hover_data=['x', 'y', 'genres'])
 #fig.show()
 
-#Avaliando o cluster
-print(pcaPipeline[1].explained_variance_ratio_.sum())#Mostra a taxa de explicando que o PCA esta dando para os dados
-print(pcaPipeline[1].explained_variance_.sum())#Mosta o quantas colunas estão sendo explicadas das 11 que tinhamos no início
+print(pcaPipeline[1].explained_variance_ratio_.sum())
+print(pcaPipeline[1].explained_variance_.sum())
 
 print(datas['artists'].value_counts())
 print(datas['artists_song'].value_counts())
 print('.')
 
-ohe = OneHotEncoder(dtype=int)#Ele faz basicamente a mesma coisa que o getDummies, porém ele é aplicado para pipelines
+ohe = OneHotEncoder(dtype=int)
 
 columnsOhe = ohe.fit_transform(datas[['artists']]).toarray()
 print(columnsOhe)
@@ -119,8 +109,7 @@ print(datasMusicDummies)
 print(datas.shape)
 print(datasMusicDummies.shape)
 
-#Aplicando o PCA
-pcaPipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=0.7, random_state=SEED))])#Neste caso foi setado a taxa de aprendizado e não o nº de colunas
+pcaPipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=0.7, random_state=SEED))])
 
 musicEmbeddingPca = pcaPipeline.fit_transform(datasMusicDummies.drop(['id', 'name', 'artists_song'], axis=1))
 projectionM = pd.DataFrame(data = musicEmbeddingPca)
@@ -142,7 +131,6 @@ fig = px.scatter_3d(projectionM, x=0, y=1, z=2, color='cluster_PCA', hover_data=
 print(pcaPipeline[1].explained_variance_ratio_.sum())
 print(pcaPipeline[1].explained_variance_.sum())
 
-#Recomendação da música
 songName = 'Ed Sheeran - Shape of You'
 cluster = list(projectionM[projectionM['song'] == songName]['cluster_PCA'])[0]
 print(cluster)
@@ -150,7 +138,6 @@ recommendedSongs = projectionM[projectionM['cluster_PCA'] == cluster][[0, 1, 'so
 xSong = list(projectionM[projectionM['song'] == songName][0])[0]
 ySong = list(projectionM[projectionM['song'] == songName][1])[0]
 
-#Distâncias euclidianas
 distances = euclidean_distances(recommendedSongs[[0, 1]], [[xSong, ySong]])
 recommendedSongs['id'] = datas['id']
 recommendedSongs['distances'] = distances
@@ -168,12 +155,9 @@ OAuth = SpotifyOAuth(
 client_credentials_manager = SpotifyClientCredentials(client_id = 'a168df4934644d899b576fec4c16c264',client_secret = '16eeb4252af54281bf311193b1cb1b99')
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-#Importando a imagem do álbum
-#Achando o Id
 songName = 'Ed Sheeran - Shape of You'
 id = datas[datas['artists_song'] == songName]['id'].iloc[0]
 
-#Fazendo a requisição na API
 track = sp.track(id)
 url = track["album"]["images"][1]["url"]
 name = track["name"]
@@ -183,7 +167,6 @@ plt.imshow(image)
 plt.xlabel(name, fontsize = 10)
 #plt.show()
 
-#Buscando os dados da playlist
 def recommend_id(playlist_id): 
     url = []
     name = []
@@ -200,39 +183,26 @@ def visualizeSongs (name, url):
     plt.figure(figsize=(15,10))
     columns = 5
     for i, u in enumerate(url):
-        # define o ax como o subplot, com a divisão que retorna inteiro do número urls pelas colunas + 1 (no caso, 6)
         ax = plt.subplot(len(url) // columns + 1, columns, i + 1)
 
-        # Lendo a imagem com o Scikit Image
         image = io.imread(u)
 
-        # Mostra a imagem
         plt.imshow(image)
 
-        # Para deixar o eixo Y invisível 
         ax.get_yaxis().set_visible(False)
 
-        # xticks define o local que vamos trocar os rótulos do eixo x, nesse caso, deixar os pontos de marcação brancos
         plt.xticks(color = 'w', fontsize = 0.1)
 
-        # yticks define o local que vamos trocar os rótulos do eixo y, nesse caso, deixar os pontos de marcação brancos
         plt.yticks(color = 'w', fontsize = 0.1)
 
-        # Colocando o nome da música no eixo x
         plt.xlabel(name[i], fontsize = 8)
 
-        # Faz com que todos os parâmetros se encaixem no tamanho da imagem definido
         plt.tight_layout(h_pad=0.7, w_pad=0)
 
-        # Ajusta os parâmetros de layout da imagem.
-        # wspace = A largura do preenchimento entre subparcelas, como uma fração da largura média dos eixos.
-        # hspace = A altura do preenchimento entre subparcelas, como uma fração da altura média dos eixos.
         plt.subplots_adjust(wspace=0.05, hspace=0.08)
 
-        # Remove os ticks - marcadores, do eixo x, sem remover o eixo todo, deixando o nome da música.
         plt.tick_params(bottom = False)
 
-        # Tirar a grade da imagem, gerada automaticamente pelo matplotlib
         plt.grid(visible=None, which='major', axis='y')
     #plt.show()
 
